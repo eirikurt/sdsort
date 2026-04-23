5cea4cd800a4c0762f4a524b10c9e2a6ead3dfa8
eirikur-nc <eirikur@neckcare.com>
2026-04-23 14:19:59 +0000

    fix: sort order is unstable when circular deps are present (#28)

--- test/cases/circular.out.py
+++ test/cases/circular.out.py
@@ -9,3 +9,15 @@ class CircularRef:
     def b(self, some_val: int):
         if some_val < 0:
             self.a(some_val // 2)
+
+
+class Selector():
+    def __or__(self, other):
+        return self.union(other)
+
+    def union(self, other):  # noqa: D102
+        match other:
+            case Selector():
+                return 1
+            case _:
+                return super().__or__(other)
